efd47751f74a6f983d24c509399bdf03c388e689
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-06 14:39:50 -0500

    Project setup

---
+++ photo-map/photo-map/Controller/MapVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo-map
+//
+//  Created by Kilyan SOCKALINGUM on 06/01/2020.
+//  Copyright © 2020 Kilyan SOCKALINGUM. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
